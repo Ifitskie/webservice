bf59987fca5710a57e43fe58eba32af1dd5a59a6
Ifitskie <ifitskie@gmail.com>
2016-10-06 14:08:29 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,5 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_password = '';
 $db_database = 'webservice';
-//
-//$db_host = 'localhost';
-//$db_user = '0892682';
-//$db_password = 'Kroko1loko!';
-//$db_database = '0892682';
 
 $connect = mysqli_connect($db_host, $db_user, $db_password, $db_database);
